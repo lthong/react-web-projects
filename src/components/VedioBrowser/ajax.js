fb0cfb7dbe10a8cd18abdc85d3542440c17c6563
lthong <appyegde58965@gmail.com>
2022-06-30 23:22:19 +0800

    add base ajax

---
+++ src/components/VedioBrowser/ajax.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 10,
+    key: process.env.YOUTUBE_AIP_KEY,
+  },
+});
