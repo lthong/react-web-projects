553d7663b605ec4fc82bf55c7b1ffb500596ffa2
lthong <appyegde58965@gmail.com>
2022-08-14 18:16:54 +0800

    add useInterval

---
+++ src/hooks/useInterval.js
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react';
+
+const useInterval = ({ update = () => {}, delay = 200, isStartInterval }) => {
+  const intervalCb = useRef(update);
+  const intervalId = useRef();
+
+  useEffect(() => {
+    intervalCb.current = update;
+  }, [update]);
+
+  useEffect(() => {
+    if (isStartInterval) {
+      intervalId.current = setInterval(intervalCb.current, delay);
+    }
+    return () => {
+      clearInterval(intervalId.current);
+    };
+  }, [isStartInterval, delay]);
+};
+
+export default useInterval;
