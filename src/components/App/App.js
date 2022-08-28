8e58affa4937dbb72f9ccc4aec4eed9bbc97b039
lthong <appyegde58965@gmail.com>
2022-08-28 22:32:47 +0800

    add page classname

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import clsx from 'clsx';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, useLocation } from 'react-router-dom';
 import * as Loadable from '@/components/App/Loadable';
 import routerPath from '@/libraries/routerPath';
 import Menu from '@/components/Menu';
@@ -16,20 +16,25 @@ const Comps = [
 
 const App = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(true);
+  const { pathname } = useLocation();
+  const pathClassname = useMemo(() => `path-${pathname.substr(1)}`, [pathname]);
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_PATH}>
-      <div className={clsx('app', { lite: !isDarkTheme })}>
-        <Menu isDarkTheme={isDarkTheme} setIsDarkTheme={setIsDarkTheme} />
-        <div className='content'>
-          {Comps.map(({ path, Comp, exact = false }) => (
-            <Route key={path} path={path} exact={exact}>
-              <Comp />
-            </Route>
-          ))}
-        </div>
+    <div
+      className={clsx('app', {
+        lite: !isDarkTheme,
+        [pathClassname]: true,
+      })}
+    >
+      <Menu isDarkTheme={isDarkTheme} setIsDarkTheme={setIsDarkTheme} />
+      <div className='content'>
+        {Comps.map(({ path, Comp, exact = false }) => (
+          <Route key={path} path={path} exact={exact}>
+            <Comp />
+          </Route>
+        ))}
       </div>
-    </BrowserRouter>
+    </div>
   );
 };
 
