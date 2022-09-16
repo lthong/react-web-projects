678dac3372185684f8f195551e4e56033f4191c3
lthong <appyegde58965@gmail.com>
2022-09-16 23:49:08 +0800

    implement the home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import clsx from 'clsx';
+import { useHistory } from 'react-router-dom';
+import pageImgs from './assets/imgs';
+import { pageConfig } from '@/libraries/routerPath';
+import { isMobile } from '@/libraries/variable';
 
 const Home = () => {
-  return <div className='home-block'>Home</div>;
+  const history = useHistory();
+  return (
+    <div className='home-block'>
+      <div className='ui four column doubling stackable grid container'>
+        {pageConfig.map(({ path, Icon, label }, i) => (
+          <div key={path} className='column'>
+            <div
+              className={clsx('ui link card', { 'small-size': isMobile })}
+              onClick={() => {
+                history.push(path);
+              }}
+            >
+              <img src={pageImgs[`${isMobile ? 'm' : 'd'}${i + 1}`]} />
+              <div className='content'>
+                <div className='header title'>
+                  <Icon size={16} className='nav-icon' />
+                  <div className='label'>{label}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
