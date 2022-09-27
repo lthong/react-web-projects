edb66dbf498dea4d93fde97dbf84ebe689baaf8f
lthong <appyegde58965@gmail.com>
2022-09-27 23:12:27 +0800

    implement the JigsawGame

--- src/components/JigsawGame/JigsawGame.js
+++ src/components/JigsawGame/JigsawGame.js
@@ -1,69 +1,178 @@
 import React, { useCallback, useState } from 'react';
-import { FaRandom } from 'react-icons/fa';
-import clsx from 'clsx';
 import jigsawImgs from './assets/imgs';
-
-const pieces = Array(4).fill(null);
+import { pieces, getRandomOriginPiece } from './utils';
 
 const JigsawGame = () => {
+  const [originPiece, setOriginPiece] = useState(getRandomOriginPiece());
+  const [targetPiece, setTargetPiece] = useState({});
+  const [currentDragPieceData, setCurrentDragPieceData] = useState({});
   const [jigsawIndex, setJigsawIndex] = useState(0);
-  const [target, setTarget] = useState({});
-  const [currentDragIndex, setCurrentDragIndex] = useState(null);
+  const [isHintOpen, setIsHintOpen] = useState(false);
 
   const changeJigsaw = useCallback(() => {
     setJigsawIndex((preState) =>
       preState + 1 >= jigsawImgs.length ? 0 : preState + 1
     );
-    setTarget({});
+    setOriginPiece(getRandomOriginPiece());
+    setTargetPiece({});
+  }, []);
+
+  const onFinish = useCallback(() => {
+    const targetPieceValues = Object.values(targetPiece);
+    const isValid =
+      targetPieceValues.length > 0 &&
+      targetPieceValues.every(
+        (pieceIndex, blockIndex) => pieceIndex === Number(blockIndex) + 1
+      );
+    const resultMsg = isValid ? '恭喜答對！' : '加油！快完成了！';
+    alert(resultMsg);
+    if (isValid) {
+      changeJigsaw();
+    }
+  }, [targetPiece, changeJigsaw]);
+
+  const onHintOpenChange = useCallback(() => {
+    setIsHintOpen((preState) => !preState);
   }, []);
 
   return (
     <div className='jigsaw-game'>
-      <FaRandom className='change-bg-icon' onClick={changeJigsaw} />
       <div className='main-block'>
+        {/* 此區塊為題目區塊 */}
         <div className='jigsaw-block'>
           {pieces.map((_, index) => (
-            <div key={`piece-${index}`} className='box'>
-              <div
-                className={clsx(`piece order-${index + 1}`)}
-                style={{ backgroundImage: `url(${jigsawImgs[jigsawIndex]})` }}
-                onDrag={(e) => {
-                  e.preventDefault();
-                  setCurrentDragIndex(index);
-                }}
-                draggable
-              ></div>
-            </div>
-          ))}
-        </div>
-        <div className='jigsaw-block'>
-          {pieces.map((_, index) => (
+            // box為放置拼圖的框框
             <div
-              key={`target-${index}`}
+              key={`originPiece-${index}`}
               className='box'
               onDragOver={(e) => {
                 e.preventDefault();
               }}
               onDrop={(e) => {
                 e.preventDefault();
-                setTarget((preState) => ({
-                  ...preState,
-                  [index]: (
-                    <div
-                      className={clsx(`piece order-${currentDragIndex + 1}`)}
-                      style={{
-                        backgroundImage: `url(${jigsawImgs[jigsawIndex]})`,
-                      }}
-                    />
-                  ),
-                }));
+                // box為空拼圖才能被放置
+                if (!originPiece[index]) {
+                  if (currentDragPieceData.type !== 'target') {
+                    // 移除來源位置的拼圖 (piece from origin block)
+                    setOriginPiece((preState) => ({
+                      ...preState,
+                      [currentDragPieceData.boxIndex]: null,
+                    }));
+                  } else {
+                    // 移除來源位置的拼圖 (piece from target block)
+                    setTargetPiece((preState) => ({
+                      ...preState,
+                      [currentDragPieceData.boxIndex]: null,
+                    }));
+                  }
+
+                  // 放置新的拼圖
+                  setOriginPiece((preState) => ({
+                    ...preState,
+                    [index]: currentDragPieceData.pieceIndex,
+                  }));
+                }
               }}
             >
-              {target[index]}
+              {originPiece[index] && (
+                // 拼圖圖層可拖曳
+                <div
+                  className={`piece order-${originPiece[index]}`}
+                  style={{ backgroundImage: `url(${jigsawImgs[jigsawIndex]})` }}
+                  onDrag={(e) => {
+                    e.preventDefault();
+                    setCurrentDragPieceData({
+                      boxIndex: index,
+                      pieceIndex: originPiece[index],
+                      type: 'origin',
+                    });
+                  }}
+                  // onMouseMove={(e) => {
+                  // const touchLocation = e.targetTouches[0];
+                  // console.log(touchLocation);
+                  // }}
+                  draggable
+                />
+              )}
+            </div>
+          ))}
+        </div>
+        {/* 此區塊為答案區塊 */}
+        <div className='jigsaw-block'>
+          {pieces.map((_, index) => (
+            <div
+              key={`targetPiece-${index}`}
+              className='box'
+              onDragOver={(e) => {
+                e.preventDefault();
+              }}
+              onDrop={(e) => {
+                e.preventDefault();
+                if (!targetPiece[index]) {
+                  if (currentDragPieceData.type !== 'target') {
+                    // 移除來源位置的拼圖 (piece from origin block)
+                    setOriginPiece((preState) => ({
+                      ...preState,
+                      [currentDragPieceData.boxIndex]: null,
+                    }));
+                  } else {
+                    // 移除來源位置的拼圖 (piece from target block)
+                    setTargetPiece((preState) => ({
+                      ...preState,
+                      [currentDragPieceData.boxIndex]: null,
+                    }));
+                  }
+
+                  // 放置新的拼圖
+                  setTargetPiece((preState) => ({
+                    ...preState,
+                    [index]: currentDragPieceData.pieceIndex,
+                  }));
+                }
+              }}
+            >
+              {targetPiece[index] && (
+                <div
+                  className={`target order-${targetPiece[index]}`}
+                  style={{ backgroundImage: `url(${jigsawImgs[jigsawIndex]})` }}
+                  onDrag={(e) => {
+                    e.preventDefault();
+                    setCurrentDragPieceData({
+                      boxIndex: index,
+                      pieceIndex: targetPiece[index],
+                      type: 'target',
+                    });
+                  }}
+                  draggable
+                />
+              )}
             </div>
           ))}
         </div>
       </div>
+      <div className='actions'>
+        <button className='ui teal button' onClick={onHintOpenChange}>
+          提示
+        </button>
+        <button className='ui green button' onClick={changeJigsaw}>
+          下一題
+        </button>
+        <button className='ui teal button' onClick={onFinish}>
+          送出答案
+        </button>
+      </div>
+      {isHintOpen && (
+        <div
+          className='hint-modal'
+          onClick={(e) => {
+            if (e.target.id !== 'hint-img') {
+              setIsHintOpen(false);
+            }
+          }}
+        >
+          <img src={jigsawImgs[jigsawIndex]} alt='hint' id='hint-img' />
+        </div>
+      )}
     </div>
   );
 };
