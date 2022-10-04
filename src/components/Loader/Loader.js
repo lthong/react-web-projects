63cd7d357b61e33c607c7745faa91dc30f0645c2
lthong <appyegde58965@gmail.com>
2022-10-04 21:33:45 +0800

    add the tw-elements

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const Loader = () => (
-  <div className='loading-bar'>
-    <div className='ui active centered inline loader' />
+  <div
+    className='mt-7 spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-gray-300'
+    role='status'
+  >
+    <span className='visually-hidden'>Loading...</span>
   </div>
 );
 
