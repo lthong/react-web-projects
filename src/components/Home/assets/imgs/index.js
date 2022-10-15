0a1faab90291953c54ad7b135c8eb02b8384646f
lthong <appyegde58965@gmail.com>
2022-10-15 13:29:52 +0800

    add some assets

--- src/components/Home/assets/imgs/index.js
+++ src/components/Home/assets/imgs/index.js
@@ -2,9 +2,11 @@ import m1 from './m1.jpg';
 import m2 from './m2.jpg';
 import m3 from './m3.jpg';
 import m4 from './m4.jpg';
+import m5 from './m5.jpg';
 import d1 from './d1.jpg';
 import d2 from './d2.jpg';
 import d3 from './d3.jpg';
 import d4 from './d4.jpg';
+import d5 from './d5.jpg';
 
-export default { m1, m2, m3, m4, d1, d2, d3, d4 };
+export default { m1, m2, m3, m4, m5, d1, d2, d3, d4, d5 };
