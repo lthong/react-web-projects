27bebbad9f058b653efa9b1bdee0dc2b448f5c5c
lthong <appyegde58965@gmail.com>
2022-10-17 23:04:30 +0800

    implement the top bar scroll style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,13 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import clsx from 'clsx';
-import { BrowserRouter, Route, useLocation } from 'react-router-dom';
+import { Route, useLocation } from 'react-router-dom';
 import * as Loadable from '@/components/App/Loadable';
 import routerPath from '@/libraries/routerPath';
 import Menu from '@/components/Menu';
 import 'semantic-ui-css/semantic.min.css';
 
 const Comps = [
-  { path: routerPath.HOME, Comp: Loadable.Home, exact: true },
+  // { path: routerPath.HOME, Comp: Loadable.Home, exact: true },
   { path: routerPath.MUSIC_PLAYER, Comp: Loadable.MusicPlayer },
   { path: routerPath.VEDIO_BROWSER, Comp: Loadable.VedioBrowser },
   { path: routerPath.IG_FILTER, Comp: Loadable.IGFilter },
@@ -17,8 +17,14 @@ const Comps = [
 
 const App = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(true);
+  const [isScrollDown, setIsScrollDown] = useState(false);
+
   const { pathname } = useLocation();
   const pathClassname = useMemo(() => `path-${pathname.substr(1)}`, [pathname]);
+
+  const onScrollDown = useCallback((isDown) => {
+    setIsScrollDown(isDown);
+  }, []);
 
   return (
     <div
@@ -27,8 +33,15 @@ const App = () => {
         [pathClassname]: true,
       })}
     >
-      <Menu isDarkTheme={isDarkTheme} setIsDarkTheme={setIsDarkTheme} />
+      <Menu
+        isDarkTheme={isDarkTheme}
+        setIsDarkTheme={setIsDarkTheme}
+        isScrollDown={isScrollDown}
+      />
       <div className='content'>
+        <Route path={routerPath.HOME} exact={true}>
+          <Loadable.Home onScrollDown={onScrollDown} />
+        </Route>
         {Comps.map(({ path, Comp, exact = false }) => (
           <Route key={path} path={path} exact={exact}>
             <Comp />
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import { useHistory } from 'react-router-dom';
 import pageImgs from './assets/imgs';
 import { pageConfig } from '@/libraries/routerPath';
 import { isMobile } from '@/libraries/variable';
 
-const Home = () => {
+const Home = ({ onScrollDown }) => {
   const history = useHistory();
+
+  useEffect(() => {
+    const onScroll = () => {
+      onScrollDown(window.scrollY > 400);
+    };
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      onScrollDown(false);
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [onScrollDown]);
+
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, []);
+
   return (
     <div className='home-block'>
+      <div className='bg'>
+        <div className='title'>Welcome To My Website</div>
+      </div>
       <div className='ui four column doubling stackable grid container'>
         {pageConfig.map(({ path, Icon, label }, i) => (
           <div key={path} className='column'>
