979b13fec3f0e8529059ae94f8ed447a22079aef
lthong <appyegde58965@gmail.com>
2022-10-17 23:03:40 +0800

    reset the Loader comp

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
 const Loader = () => (
-  <div
-    className='mt-7 spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-gray-300'
-    role='status'
-  >
-    <span className='visually-hidden'>Loading...</span>
+  <div className='loading-bar'>
+    <div className='ui active centered inline loader' />
   </div>
 );
 
--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,3 +1,4 @@
 import Loader from './Loader';
+import './style.scss';
 
 export default Loader;
