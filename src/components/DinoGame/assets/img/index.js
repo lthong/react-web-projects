7c78a9c8ca090694a2862e8fb57fcf6fe75a614b
lthong <appyegde58965@gmail.com>
2022-11-04 21:01:57 +0800

    implement the DinoGame

---
+++ src/components/DinoGame/assets/img/index.js
@@ -0,0 +1,6 @@
+import groundImg from './ground.png';
+import dinoRun0 from './dino-run-0.png';
+import dinoRun1 from './dino-run-1.png';
+import dinoInit from './dino-init.png';
+
+export { groundImg, dinoRun0, dinoRun1, dinoInit };
