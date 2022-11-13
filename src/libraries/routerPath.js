b15d46fb08a4c2563cf6407bf321119c0c4be7e0
lthong <appyegde58965@gmail.com>
2022-11-13 22:48:27 +0800

    update the home page

--- src/libraries/routerPath.js
+++ src/libraries/routerPath.js
@@ -29,6 +29,7 @@ export const navs = [
     Icon: BsMusicNoteBeamed,
     type: 'service',
     imgName: 'm1',
+    intro: '小小的音樂播放器，支援切換曲目，即時顯示目前播放的音軌位置及時間',
   },
   {
     path: pages.VEDIO_BROWSER,
@@ -36,6 +37,8 @@ export const navs = [
     Icon: AiFillYoutube,
     type: 'service',
     imgName: 'm2',
+    intro:
+      '簡易版 Youtube，有支援無限滾動喔！不過因為 API 有每日請求上限，當作一個 demo 就好啦 ＸＤ',
   },
   {
     path: pages.IG_FILTER,
@@ -43,6 +46,7 @@ export const navs = [
     Icon: BsFillCameraFill,
     type: 'tool',
     imgName: 'm3',
+    intro: '簡易的 IG 濾鏡，可以自己上傳檔案，並支援下載儲存',
   },
   {
     path: pages.SNAKE_GAME,
@@ -50,6 +54,7 @@ export const navs = [
     Icon: GiSnakeSpiral,
     type: 'game',
     imgName: 'm4',
+    intro: '經典的貪吃蛇遊戲，小心不要碰到牆壁與蛇身喔！',
   },
   {
     path: pages.JIGSAW_GAME,
@@ -57,6 +62,7 @@ export const navs = [
     Icon: GiJigsawPiece,
     type: 'game',
     imgName: 'm5',
+    intro: '要不要玩拼圖～支援桌機與手機的拖拉模式，實作難度五顆心ＸＤ',
   },
   {
     path: pages.DINO_GAME,
@@ -64,6 +70,7 @@ export const navs = [
     Icon: FaGamepad,
     type: 'game',
     imgName: 'm6',
+    intro: '復刻 Google 經典的恐龍遊戲，還有背景音樂喔！',
   },
 ];
 
