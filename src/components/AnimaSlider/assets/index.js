2a004f424627994b1ab45c04801ca006397a81b5
lthong <appyegde58965@gmail.com>
2022-11-27 23:12:50 +0800

    implement the anima slider

---
+++ src/components/AnimaSlider/assets/index.js
@@ -0,0 +1,25 @@
+import spy1 from './spyFamily/1.jpg';
+import spy2 from './spyFamily/2.jpg';
+import spy3 from './spyFamily/3.jpg';
+import spy4 from './spyFamily/4.jpg';
+import spy5 from './spyFamily/5.jpg';
+import spy6 from './spyFamily/6.jpg';
+import spyBanner from './spyFamily/banner.jpg';
+import himeBanner from './hime/banner.jpg';
+import campBanner from './camp/banner.jpg';
+import tadakoiBanner from './tadakoi/banner.jpg';
+import silverSpoonBanner from './silverSpoon/banner.jpg';
+
+export {
+  spy1,
+  spy2,
+  spy3,
+  spy4,
+  spy5,
+  spy6,
+  spyBanner,
+  himeBanner,
+  campBanner,
+  tadakoiBanner,
+  silverSpoonBanner,
+};
