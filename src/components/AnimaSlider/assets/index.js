6242772dfd124c4f07dfa9b04cd69ee5da6b3f6c
lthong <appyegde58965@gmail.com>
2022-11-28 22:35:59 +0800

    implement the anima slider

--- src/components/AnimaSlider/assets/index.js
+++ src/components/AnimaSlider/assets/index.js
@@ -4,19 +4,54 @@ import spy3 from './spyFamily/3.jpg';
 import spy4 from './spyFamily/4.jpg';
 import spy5 from './spyFamily/5.jpg';
 import spy6 from './spyFamily/6.jpg';
+import hime1 from './hime/1.jpg';
+import hime2 from './hime/2.jpg';
+import hime3 from './hime/3.jpg';
+import hime4 from './hime/4.jpg';
+import hime5 from './hime/5.jpg';
+import hime6 from './hime/6.jpg';
+import camp1 from './camp/1.jpeg';
+import camp2 from './camp/2.jpeg';
+import camp3 from './camp/3.jpeg';
+import camp4 from './camp/4.jpeg';
+import camp5 from './camp/5.jpeg';
+import camp6 from './camp/6.jpeg';
+import tadakoi1 from './tadakoi/1.jpeg';
+import tadakoi2 from './tadakoi/2.jpeg';
+import tadakoi3 from './tadakoi/3.jpeg';
+import tadakoi4 from './tadakoi/4.jpeg';
+import tadakoi5 from './tadakoi/5.jpeg';
+import tadakoi6 from './tadakoi/6.jpeg';
+import silverSpoon1 from './silverSpoon/1.jpeg';
+import silverSpoon2 from './silverSpoon/2.jpeg';
+import silverSpoon3 from './silverSpoon/3.jpeg';
+import silverSpoon4 from './silverSpoon/4.jpeg';
+import silverSpoon5 from './silverSpoon/5.jpeg';
+import silverSpoon6 from './silverSpoon/6.jpeg';
 import spyBanner from './spyFamily/banner.jpg';
 import himeBanner from './hime/banner.jpg';
 import campBanner from './camp/banner.jpg';
 import tadakoiBanner from './tadakoi/banner.jpg';
 import silverSpoonBanner from './silverSpoon/banner.jpg';
+const spy = [spy1, spy2, spy3, spy4, spy5, spy6];
+const hime = [hime1, hime2, hime3, hime4, hime5, hime6];
+const camp = [camp1, camp2, camp3, camp4, camp5, camp6];
+const tadakoi = [tadakoi1, tadakoi2, tadakoi3, tadakoi4, tadakoi5, tadakoi6];
+const silverSpoon = [
+  silverSpoon1,
+  silverSpoon2,
+  silverSpoon3,
+  silverSpoon4,
+  silverSpoon5,
+  silverSpoon6,
+];
 
 export {
-  spy1,
-  spy2,
-  spy3,
-  spy4,
-  spy5,
-  spy6,
+  spy,
+  hime,
+  camp,
+  tadakoi,
+  silverSpoon,
   spyBanner,
   himeBanner,
   campBanner,
