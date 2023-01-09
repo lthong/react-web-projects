10cd5ebb848b6928e94518c2a15b8262f1239dce
lthong <appyegde58965@gmail.com>
2023-01-09 23:09:40 +0800

    implement the Drawing

--- src/libraries/variable.js
+++ src/libraries/variable.js
@@ -1,1 +1,11 @@
 export const isMobile = window.innerWidth < 530;
+
+const topHeight = 55;
+const canvasW = window.innerWidth;
+const canvasH = window.innerHeight - topHeight;
+
+export const fullCanvas = {
+  topHeight,
+  canvasW,
+  canvasH,
+};
