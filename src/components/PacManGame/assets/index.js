c682ee3e35cc3e607087718d698b697794bfada4
lthong <appyegde58965@gmail.com>
2023-01-24 14:42:02 +0800

    implement the pacman game

---
+++ src/components/PacManGame/assets/index.js
@@ -0,0 +1,35 @@
+import pipeHorizontal from './pipeHorizontal.png';
+import pipeVertical from './pipeVertical.png';
+import pipeCorner1 from './pipeCorner1.png';
+import pipeCorner2 from './pipeCorner2.png';
+import pipeCorner3 from './pipeCorner3.png';
+import pipeCorner4 from './pipeCorner4.png';
+import block from './block.png';
+import capLeft from './capLeft.png';
+import capRight from './capRight.png';
+import capBottom from './capBottom.png';
+import capTop from './capTop.png';
+import pipeCross from './pipeCross.png';
+import pipeConnectorTop from './pipeConnectorTop.png';
+import pipeConnectorRight from './pipeConnectorRight.png';
+import pipeConnectorBottom from './pipeConnectorBottom.png';
+import pipeConnectorLeft from './pipeConnectorLeft.png';
+
+export {
+  pipeHorizontal,
+  pipeVertical,
+  pipeCorner1,
+  pipeCorner2,
+  pipeCorner3,
+  pipeCorner4,
+  block,
+  capLeft,
+  capRight,
+  capBottom,
+  capTop,
+  pipeCross,
+  pipeConnectorTop,
+  pipeConnectorRight,
+  pipeConnectorBottom,
+  pipeConnectorLeft,
+};
