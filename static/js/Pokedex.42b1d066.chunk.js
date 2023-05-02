3f55ae075761a181691aec207291a341173b0132
lthong <appyegde58965@gmail.com>
2023-05-02 08:02:45 +0800

    Updates

--- static/js/Pokedex.42b1d066.chunk.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{124:function(e,n,t){},139:function(e,n,t){"use strict";t.r(n);var a=t(1),i=t.n(a),o=function(){return i.a.createElement("div",{className:"pokedex"},i.a.createElement("iframe",{importance:"high",src:"https://lthong.github.io/pokedex/"}))};t(124),n.default=o}}]);
